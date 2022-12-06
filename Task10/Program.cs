22e8b89d7b6e5a1b2cb59f2b3921796294b64345
mikulinskiy <32305540+andrewm8888@users.noreply.github.com>
2022-12-06 22:10:38 +0300

    commit 2

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,7 +4,9 @@
 //782 -> 72
 //918 -> 98
 
-int num = new Random().Next(100, 1000);
-int a1 = num / 100;
-int a2 = num % 10;
-Console.WriteLine($"Число: {a1 * 10 + a2}");
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
